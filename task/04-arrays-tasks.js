e51a489a06b373bb90c1146aa103a8cfdd7b39fd
Lapochka15 <47964016+Lapochka15@users.noreply.github.com>
2019-03-13 19:11:19 +0300

    Update 04-arrays-tasks.js

--- task/04-arrays-tasks.js
+++ task/04-arrays-tasks.js
@@ -1,4 +1,4 @@
-u'use strict';
+'use strict';
 
 /*********************************************************************************************
  *                                                                                           *
@@ -246,7 +246,7 @@ function toArrayOfSquares(arr) {
  *   [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ] => [ 1, 3, 6, 10, 15, 21, 28, 36, 45, 55 ]
  */
 function getMovingSum(arr) {
-   throw new Error('Not implemented');
+   return (Array.from(arr, (x,k) =>  (k > 0 ? arr[k-1]: 0) + x);
 }
 
 /**
