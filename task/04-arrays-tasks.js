0f5665a52797e4a2965c8d66b6512c9e8a1ce345
Lapochka15 <47964016+Lapochka15@users.noreply.github.com>
2019-03-13 19:29:19 +0300

    Update 04-arrays-tasks.js

--- task/04-arrays-tasks.js
+++ task/04-arrays-tasks.js
@@ -246,7 +246,7 @@ function toArrayOfSquares(arr) {
  *   [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ] => [ 1, 3, 6, 10, 15, 21, 28, 36, 45, 55 ]
  */
 function getMovingSum(arr) {
-   return (Array.from(arr, (x,k) =>  (k > 0 ? arr[k-1]: 0) + x);
+   return (Array.from(arr, (x,k) =>  (k > 0 ? arr[k-1]: 0) + x));
 }
 
 /**
@@ -280,7 +280,7 @@ function getSecondItems(arr) {
  *  [ 1,2,3,4,5 ] => [ 1, 2,2, 3,3,3, 4,4,4,4, 5,5,5,5,5 ]
  */
 function propagateItemsByPositionIndex(arr) {
-   throw new Error('Not implemented');
+ throw new Error('Not implemented');
 }
 
 
