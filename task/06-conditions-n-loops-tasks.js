986617a0b9aa956b90d396a353eb3be0d7990650
Lapochka15 <47964016+Lapochka15@users.noreply.github.com>
2019-03-15 12:52:20 +0300

    Update 06-conditions-n-loops-tasks.js

--- task/06-conditions-n-loops-tasks.js
+++ task/06-conditions-n-loops-tasks.js
@@ -465,10 +465,26 @@ function getMatrixProduct(m1, m2) {
  *    [    ,   ,    ]]
  *
  */
-function evaluateTicTacToePosition(position) {
-    throw new Error('Not implemented');
+function evaluateTicTacToePosition(board) {
+   for (var i = 0; i < 3; ++i){
+      if (board[i][0] == board[i][1] && board[i][2] == board[i][1] && board[i][0] !== undefined)
+          return board[i][0];
+          
+      if (board[0][i] == board[1][i] && board[2][i] == board[1][i] && board[0][i] !== undefined)
+          return board[0][i];
+   }
+   if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] !== undefined)
+      return board[0][0];
+     
+   if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[2][0] !== undefined)
+      return board[0][2];
+     
+   for (var i = 0; i < 3; ++i)
+      if (board[i][0] == 0 || board[i][2] == 0 || board[i][1] == undefined)
+         return undefined;
+   return undefined;
+     
 }
-
 
 module.exports = {
     getFizzBuzz: getFizzBuzz,
