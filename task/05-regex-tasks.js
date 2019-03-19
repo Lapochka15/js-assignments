d8cccc6f54ecbaa8698f42637f406f863885f76c
Lapochka15 <47964016+Lapochka15@users.noreply.github.com>
2019-03-19 23:34:54 +0300

    Update 05-regex-tasks.js

--- task/05-regex-tasks.js
+++ task/05-regex-tasks.js
@@ -73,7 +73,7 @@ function getRegexForPitSpot() {
  */
 function getRegexForIPv4() {
    let number = '(25[0-5]|2[0-4][0-9]|[01]?[0-9]{1,2})';
-   new RegExp('^' + number +'[.]' +number +'[.]' +number +'[.]'+number +'$');
+   return new RegExp('^' + number +'[.]' +number +'[.]' +number +'[.]'+number +'$');
 
 }
 
